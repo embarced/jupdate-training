c74d145dcbae9bd5974bd5ab64260c4a215fa0e1
Falk Sippach <falk@jug-da.de>
2022-11-18 18:03:58 +0100

    erste Übungen

---
+++ 01-modules-A/src/jupdate/hello/Hello.java
@@ -0,0 +1,7 @@
+package jupdate.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 11 - Version 2");
+    }
+}
---
+++ 01-modules-A/src/main/java/jupdate/modules/Hello.java
@@ -0,0 +1,7 @@
+package jupdate.modules;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 11");
+    }
+}
